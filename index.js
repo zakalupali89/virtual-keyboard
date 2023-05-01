1f1956138c2c726ff42e08907bbeffd6bdc76127
Kirill <zakalupali@gmail.com>
2023-05-01 20:09:43 +0300

    feat: generation keyboard with events

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 import buttonsData from './buttons-data.js';
 
-// const textArea = document.createElement('textarea');
-// textArea.className = 'board__text-area';
-//
-// const boardContent = document.createElement('div');
-// board.className = 'board__content'
+const board = document.querySelector('.board');
 
-const textArea = document.querySelector('.board__text-area');
-const board = document.querySelector('.board__content');
+const boardContent = document.createElement('div');
+boardContent.className = 'board__content';
+
+const textArea = document.createElement('textarea');
+textArea.className = 'board__text-area';
+
+board.append(textArea, boardContent);
+board.append(boardContent);
+
+const arrFromButtonsData = Object.entries(buttonsData);
+for (let i = 0; i < 65; i += 1) {
+  const button = document.createElement('button');
+  const keyObject = arrFromButtonsData[i][0];
+  button.id = keyObject;
+  const data = buttonsData[keyObject];
+  button.className = `button-key ${data?.class || ''}`;
+  if (data.en[1] && !keyObject.includes('Key')) {
+    const spanUpName = document.createElement('span');
+    spanUpName.className = 'button-key__up-name';
+    spanUpName.innerText = buttonsData[keyObject].en[1];
+    button.append(spanUpName);
+  }
+  const spanText = document.createElement('span');
+  spanText.className = 'button-key__text';
+  spanText.innerText = buttonsData[keyObject].en[0];
+  button.append(spanText);
+  boardContent.append(button);
+}
+
+const description = document.createElement('div');
+description.className = 'description';
+description.innerText = 'Press Ctrl + Shift for changing language!';
+board.append(description);
 
 function addText(target, textInput, isShiftKeyPressed) {
   if (isShiftKeyPressed) {
@@ -22,19 +49,79 @@ function addText(target, textInput, isShiftKeyPressed) {
   }
 }
 
+function deleteChar(target, textInput, direction) {
+  const arrFromText = textArea.value.split('');
+  const { selectionStart } = textArea;
+  const { selectionEnd } = textArea;
+  if (direction === 'delete') {
+    if (selectionStart === selectionEnd) {
+      arrFromText.splice(selectionStart, 1);
+      textInput.value = arrFromText.join('');
+      textInput.selectionStart = selectionStart;
+    } else {
+      arrFromText.splice(selectionStart, selectionEnd - selectionStart);
+      textInput.value = arrFromText.join('');
+      textInput.selectionStart = selectionStart;
+    }
+  } else if (selectionStart === selectionEnd) {
+    arrFromText.splice(selectionStart - 1, 1);
+    textInput.value = arrFromText.join('');
+    textInput.selectionStart = selectionStart - 1;
+  } else {
+    arrFromText.splice(selectionStart, selectionEnd - selectionStart);
+    textInput.value = arrFromText.join('');
+    textInput.selectionStart = selectionStart;
+  }
+  textInput.selectionEnd = textInput.selectionStart;
+}
+
+function newLine(target, textInput) {
+  const arrFromText = textArea.value.split('');
+  const { selectionStart } = textArea;
+  const { selectionEnd } = textArea;
+  if (selectionStart === selectionEnd) {
+    arrFromText.splice(selectionStart, 0, '\n');
+    textInput.value = arrFromText.join('');
+    textInput.selectionStart = selectionStart + 1;
+    textInput.selectionEnd = selectionStart + 1;
+  } else {
+    arrFromText.splice(selectionStart, selectionEnd - selectionStart, '\n');
+    textInput.value = arrFromText.join('');
+    textInput.selectionStart = selectionStart + 1;
+    textInput.selectionEnd = selectionStart + 1;
+  }
+}
+
 let timer;
 let interval;
-board.onmousedown = (event) => {
+boardContent.onmousedown = (event) => {
   const { target } = event;
+  console.log(event.getModifierState('CapsLock'));
   if (target.tagName === 'BUTTON') {
-    addText(target, textArea, event.shiftKey);
-    timer = setTimeout(() => {
-      interval = setInterval(() => addText(target, textArea, event.shiftKey), 50);
-    }, 500);
+    switch (target.id) {
+      case 'Delete': {
+        deleteChar(target, textArea, 'delete');
+        break;
+      }
+      case 'Backspace': {
+        deleteChar(target, textArea, 'backspace');
+        break;
+      }
+      case 'Enter': {
+        newLine(target, textArea);
+        break;
+      }
+      default: {
+        addText(target, textArea, event.shiftKey);
+        timer = setTimeout(() => {
+          interval = setInterval(() => addText(target, textArea, event.shiftKey), 50);
+        }, 500);
+      }
+    }
   }
 };
 
-board.onmouseup = () => {
+boardContent.onmouseup = () => {
   clearTimeout(timer);
   clearInterval(interval);
   textArea.focus();
@@ -42,17 +129,14 @@ board.onmouseup = () => {
 
 document.onkeydown = (event) => {
   textArea.focus();
-  const { code, key, isTrusted } = event;
-  if (isTrusted && key !== 'Alt' && key !== 'Control' && key !== 'Shift') {
-    const buttonKey = document.getElementById(code);
-    buttonKey.classList.add('active');
-  }
+  const { code, key } = event;
+  console.log(code, key);
+  const buttonKey = document.getElementById(code);
+  buttonKey.classList.add('active');
 };
 
 document.onkeyup = (event) => {
-  const { code, key } = event;
-  if (key !== 'Alt' && key !== 'Control' && key !== 'Shift') {
-    const buttonKey = document.getElementById(code);
-    buttonKey.classList.remove('active');
-  }
+  const { code } = event;
+  const buttonKey = document.getElementById(code);
+  buttonKey.classList.remove('active');
 };
